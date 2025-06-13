be5b51989a302d731ee8b89e04151523285086a1
Laksh Sachdeva <lakshsachdeva01@example.com>
2025-06-13 16:11:20 +0530

    updated tailwind paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // adjust based on your structure
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,13 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import autoprefixer from "autoprefixer"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss() , autoprefixer()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      '@/components': path.resolve(__dirname, './components'),
+      "@/components": path.resolve(__dirname, "./components"),
     },
   },
-})
+});
